5194ad8d5371910cc0d2f2762920c3f475cd026c
Christopher Moore <christopher.e.moore@gmail.com>
2014-08-01 10:42:59 -0700

    first commit

---
+++ call.lua
@@ -0,0 +1,17 @@
+local gl = require 'ffi.OpenGL'
+
+local function glCallOrRun(list, callback, ...)
+	assert(type(list) == 'table', "expected first parameter to be a table to store the list id")
+	if list.id then
+		gl.glCallList(list.id)
+	else
+		list.id = gl.glGenLists(1)
+		gl.glNewList(list.id, gl.GL_COMPILE_AND_EXECUTE)
+		
+		callback(...)
+		
+		gl.glEndList()
+	end
+end
+
+return glCallOrRun
---
+++ intersect.lua
@@ -0,0 +1,44 @@
+local ffi = require 'ffi'
+local gl = require 'ffi.OpenGL'
+local glu = require 'ffi.glu'
+
+-- glSceneIntersect
+
+local readBufferPtr = ffi.new('GLint[1]')
+local depthValuePtr = ffi.new('GLfloat[1]')
+local modelC = ffi.new('GLdouble[16]')
+local projC = ffi.new('GLdouble[16]')
+local viewC = ffi.new('GLint[4]')
+local objX = ffi.new('GLdouble[1]')
+local objY = ffi.new('GLdouble[1]')
+local objZ = ffi.new('GLdouble[1]')
+local function glSceneIntersect(screenFracPosX, screenFracPosY, renderCallback, ...)
+	gl.glGetIntegerv(gl.GL_READ_BUFFER, readBufferPtr)
+	local readbuf = readBufferPtr[0]
+	gl.glReadBuffer(gl.GL_BACK)
+
+	if renderCallback then renderCallback(...) end
+
+	gl.glGetIntegerv(gl.GL_VIEWPORT, viewC)
+	local mx = math.floor(screenFracPosX * (tonumber(viewC[2]) - 1))
+	local my = math.floor(screenFracPosY * (tonumber(viewC[3]) - 1))
+	
+	gl.glReadPixels(mx, my, 1, 1, gl.GL_DEPTH_COMPONENT, gl.GL_FLOAT, depthValuePtr)
+	local pix = depthValuePtr[0]
+	
+	--if pix == 1 then return nil end	-- full depth means a cleared-depth value, means nothing was here
+	
+	gl.glGetDoublev(gl.GL_MODELVIEW_MATRIX, modelC)
+	gl.glGetDoublev(gl.GL_PROJECTION_MATRIX, projC)
+	
+	local res = glu.gluUnProject(mx, my, pix, modelC, projC, viewC, objX, objY, objZ)
+	local projX = objX[0]
+	local projY = objY[0]
+	local projZ = objZ[0]
+
+	gl.glReadBuffer(readbuf)
+	
+	return projX, projY, projZ, pix
+end
+
+return glSceneIntersect
