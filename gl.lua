6f3a0bce49d9101cb4e2e639f6ac357fd9bd13b0
Christopher Moore <christopher.e.moore@gmail.com>
2023-07-24 21:13:26 +0700

    no more global

--- gl.lua
+++ gl.lua
@@ -1,3 +1,3 @@
--- TODO more modular or something?  I don't like globals...
--- use require 'gl.setup' to avoid using a global
-return require(ffi_OpenGL or 'ffi.OpenGL')
+-- this is the default ffi library to use
+-- to override this, use require 'gl.setup'(glname)
+return require 'ffi.OpenGL'
