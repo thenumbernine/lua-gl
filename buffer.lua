97bd98eea450e265af8b91bff49ee28178d443c3
Christopher Moore <christopher.e.moore@gmail.com>
2023-09-18 17:45:46 +0700

    more

--- buffer.lua
+++ buffer.lua
@@ -15,9 +15,10 @@ local Buffer = class(GCWrapper{
 
 --[[
 args:
-	size
-	data (optional)
-	usage
+	size = forwarded to setData
+	data (optional) = forwarded to setData
+	usage (optional) = forwarded to setData
+	target (optional) = this is forwarded to setData, but unlike the other fields it is not assigned to self
 my js version has dim and count instead of size, then just size = dim * count
 this makes it more compatible with GLProgram:setAttr(buffer)
 instead of only GLProgram:setAttr(attr)
@@ -27,6 +28,7 @@ function Buffer:init(args)
 	gl.glGenBuffers(1, self.gc.ptr)
 	self.id = self.gc.ptr[0]
 
+	-- TODO bind even if we have no args?  or only if args are provided / setData is called?
 	self:bind()
 	if args then
 		if args.data then
@@ -54,6 +56,7 @@ args:
 	data = (optional) data, default is null.
 	usage = (optional) GL buffer usage.  default GL_STATIC_DRAW.
 	type = (optional) if data is a Lua table, this specifies what c type to convert it to.  default float.
+	target = (optional) override self.target in the glBindBuffer call, but unlike the other fields it does not assign to self.
 my js version has 'keep' for saving args.data ... I'll just make it default
 --]]
 function Buffer:setData(args)
@@ -74,7 +77,7 @@ function Buffer:setData(args)
 	self.data = data
 	self.size = size
 	self.usage = args.usage or gl.GL_STATIC_DRAW
-	gl.glBufferData(self.target, self.size, self.data, self.usage)
+	gl.glBufferData(args.target or self.target, self.size, self.data, self.usage)
 	return self
 end
 
