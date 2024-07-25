77090b34d524170462ff9720ae89b43e435194cc
thenumbernine <christopher.e.moore@gmail.com>
2024-07-25 08:44:28 +0600

    changing default element index buffer convert-from-lua-to-native type for es only

--- elementarraybuffer.lua
+++ elementarraybuffer.lua
@@ -4,8 +4,12 @@ local Buffer = require 'gl.buffer'
 local ElementArrayBuffer = Buffer:subclass()
 
 -- default data type when converting from lua buffer?
--- TODO change this depending on if it's gles (short) or not (int) ?
-ElementArrayBuffer.type = gl.GL_UNSIGNED_INT
+-- change this depending on if it's gles (short) or not (int) ?  but only if ES doesn't have its element_index_uint extension?
+if op.safeindex(gl, 'GL_ES_VERSION_2_0') then
+	ElementArrayBuffer.type = gl.GL_UNSIGNED_SHORT
+else
+	ElementArrayBuffer.type = gl.GL_UNSIGNED_INT
+end
 
 ElementArrayBuffer.target = gl.GL_ELEMENT_ARRAY_BUFFER
 
