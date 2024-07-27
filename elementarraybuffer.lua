690ff179149c5777e4bcf59ce9363b09dc38685f
thenumbernine <christopher.e.moore@gmail.com>
2024-07-27 19:08:38 +0600

    more

--- elementarraybuffer.lua
+++ elementarraybuffer.lua
@@ -1,16 +1,17 @@
-local op = require 'ext.op'
 local gl = require 'gl'
 local Buffer = require 'gl.buffer'
 
 local ElementArrayBuffer = Buffer:subclass()
 
--- default data type when converting from lua buffer?
--- change this depending on if it's gles (short) or not (int) ?  but only if ES doesn't have its element_index_uint extension?
-if op.safeindex(gl, 'GL_ES_VERSION_2_0') then
-	ElementArrayBuffer.type = gl.GL_UNSIGNED_SHORT
-else
-	ElementArrayBuffer.type = gl.GL_UNSIGNED_INT
-end
+-- Default data type when converting from lua buffer?
+-- Change this depending on if it's gles (short) or not (int) ?  but only if ES doesn't have its element_index_uint extension?
+-- Or only switch it to short if we've initialized webgl1 instead, so do that there and not here.
+--local op = require 'ext.op'
+--if op.safeindex(gl, 'GL_ES_VERSION_2_0') then
+--	ElementArrayBuffer.type = gl.GL_UNSIGNED_SHORT
+--else
+ElementArrayBuffer.type = gl.GL_UNSIGNED_INT
+--end
 
 ElementArrayBuffer.target = gl.GL_ELEMENT_ARRAY_BUFFER
 
