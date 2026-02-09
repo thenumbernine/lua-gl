1720532e5cacb5999c2076f34ca3dfe6e7bce111
Christopher Moore <christopher.e.moore@gmail.com>
2026-02-09 23:00:16 +0700

    allow overriding of gl.setup default location

--- setup.lua
+++ setup.lua
@@ -5,10 +5,17 @@ it'll setup the gl and gl.gl package to whatever you specify
 no more need for globals beyond the package.loaded global
 from then on `require 'gl'` or `require 'gl.gl'` will give you the requested GL library (found in the ffi folder)
 --]]
-return function(glname)
-	glname = glname or 'OpenGL'
-	local gl = require('gl.ffi.'..glname)
-	package.loaded.gl = gl
-	package.loaded['gl.gl'] = gl
-	return gl
-end
+return setmetatable(
+	{
+		default = 'OpenGL',
+	},
+	{
+		__call = function(self, glname)
+			glname = glname or self.default
+			local gl = require('gl.ffi.'..glname)
+			package.loaded.gl = gl
+			package.loaded['gl.gl'] = gl
+			return gl
+		end,
+	}
+)
